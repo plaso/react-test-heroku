bc115aef2098a61d46f830cc183feeb52c6cb071
Pablo Laso Cortabitarte <pablo.lasocortabitarte@telefonica.com>
2019-04-11 12:44:20 +0200

    Updates

---
+++ precache-manifest.5e124c294ff7cb76b120dd883dacde4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test-heroku/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "70747c55ea278fcd7cc1",
+    "url": "/react-test-heroku/static/js/runtime~main.e4b63f4d.js"
+  },
+  {
+    "revision": "57dd632ceb10b70e1788",
+    "url": "/react-test-heroku/static/js/main.29028aad.chunk.js"
+  },
+  {
+    "revision": "18188428f8b3af73fe72",
+    "url": "/react-test-heroku/static/js/2.afb89262.chunk.js"
+  },
+  {
+    "revision": "57dd632ceb10b70e1788",
+    "url": "/react-test-heroku/static/css/main.2d8c6b6e.chunk.css"
+  },
+  {
+    "revision": "1556fef71a9dabc5ddf9e5f57f504ecd",
+    "url": "/react-test-heroku/index.html"
+  }
+];
